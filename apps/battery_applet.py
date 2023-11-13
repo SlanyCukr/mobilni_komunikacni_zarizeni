cc24af4995091218247629edbdfdcf0643ab9faf
slanycukr <marek.hovadik@seznam.cz>
2023-11-13 09:59:52 +0100

    Saving progress

--- apps/battery_applet.py
+++ apps/battery_applet.py
@@ -1,23 +1,46 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import subprocess
 import os
 
 
-def update_battery_info():
-    # Read battery information
-    # Update the icon and percentage
-    root.after(60000, update_battery_info)  # Update every minute
+def update_battery_status():
+    # This function should read the INA219 data and return the battery percentage
+    # For now, we're using a placeholder value. Replace this with your INA219 data reading logic.
+    return 50  # Placeholder for battery percentage
+
+
+def update_icon():
+    # Update the battery icon based on the current battery status
+    battery_percentage = update_battery_status()
+
+    if battery_percentage > 75:
+        img_path = "../images/full-battery.png"  # Replace with your full battery icon path
+    elif battery_percentage > 50:
+        img_path = "../images/half-battery.png"  # Replace with your half battery icon path
+    else:
+        img_path = "../images/low-battery.png"   # Replace with your low battery icon path
+
+    img = Image.open(img_path)
+    img = img.resize((16, 16), Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(img)
+
+    tray_icon.config(image=photo)
+    tray_icon.image = photo  # Keep a reference
+    root.after(60000, update_icon)  # Update the icon every 60 seconds
+
 
 root = tk.Tk()
-root.overrideredirect(True)  # Hide the window border
-root.geometry("+X+Y")  # Replace X and Y with the coordinates for the top right corner
+root.withdraw()  # Hide the main window
 
-# Load your battery icon
-img = Image.open("../images/battery.png")
-imgTk = ImageTk.PhotoImage(img)
+# Create a system tray icon
+img = Image.open("default_battery_icon.png")  # Replace with your default battery icon path
+img = img.resize((16, 16), Image.ANTIALIAS)
+icon = ImageTk.PhotoImage(img)
 
-label = tk.Label(root, image=imgTk)
-label.pack()
+tray_icon = tk.Label(root, image=icon)
+tray_icon.pack()
+tray_icon.after(1000, update_icon)  # Update icon after 1 second to show initial status
 
-update_battery_info()
+# Run the application
 root.mainloop()
