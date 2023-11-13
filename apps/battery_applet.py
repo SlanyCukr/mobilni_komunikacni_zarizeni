065463b377e4c2e4bc47329c2faea545f1fa0c60
slanycukr <marek.hovadik@seznam.cz>
2023-11-13 10:13:43 +0100

    Saving progress

--- apps/battery_applet.py
+++ apps/battery_applet.py
@@ -1,46 +1,72 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-import subprocess
+#! /usr/bin/env python
+from glob import glob
+import gtk
+import gobject
 import os
+import os.path
+from string import rstrip
+import sys
+
+INTERVAL = 20000
+IMAGE_LOC = os.path.join(os.path.dirname(sys.argv[0]), "images/battery.png")
 
 
-def update_battery_status():
-    # This function should read the INA219 data and return the battery percentage
-    # For now, we're using a placeholder value. Replace this with your INA219 data reading logic.
-    return 50  # Placeholder for battery percentage
+class BatteryTray:
+    def __init__(self):
+        self.tray = gtk.StatusIcon()
+        self.tray.connect('activate', self.refresh)
 
+        # Create menu
+        menu = gtk.Menu()
+        i = gtk.MenuItem("About...")
+        i.show()
+        i.connect("activate", self.show_about)
+        menu.append(i)
+        i = gtk.MenuItem("Quit")
+        i.show()
+        i.connect("activate", self.quit)
+        menu.append(i)
+        self.tray.connect('popup-menu', self.show_menu, menu)
 
-def update_icon():
-    # Update the battery icon based on the current battery status
-    battery_percentage = update_battery_status()
+        # Initalise and start battery display
+        self.refresh(None)
+        self.tray.set_visible(True)
+        gobject.timeout_add(INTERVAL, self.refresh, False)
 
-    if battery_percentage > 75:
-        img_path = "../images/full-battery.png"  # Replace with your full battery icon path
-    elif battery_percentage > 50:
-        img_path = "../images/half-battery.png"  # Replace with your half battery icon path
-    else:
-        img_path = "../images/low-battery.png"   # Replace with your low battery icon path
+    def show_menu(self, widget, event_button, event_time, menu):
+        menu.popup(None, None,
+            gtk.status_icon_position_menu,
+            event_button,
+            event_time,
+            self.tray
+        )
 
-    img = Image.open(img_path)
-    img = img.resize((16, 16), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(img)
+    def show_about(self, widget):
+        dialog = gtk.MessageDialog(
+            None,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_INFO,
+            gtk.BUTTONS_OK,
+            """
+It's a battery meter
+By Jamie Lentin
+""")
+        dialog.run()
+        dialog.destroy()
 
-    tray_icon.config(image=photo)
-    tray_icon.image = photo  # Keep a reference
-    root.after(60000, update_icon)  # Update the icon every 60 seconds
+    def quit(self, widget):
+        gtk.main_quit()
 
+    def refresh(self, widget):
+        b_level = 50
+        b_file = IMAGE_LOC
+        self.tray.set_tooltip(f"{b_level}")
+        if os.path.exists(b_file):
+            self.tray.set_from_file(b_file)
+        self.tray.set_blinking(b_level <= 5)
+        return True
 
-root = tk.Tk()
-root.withdraw()  # Hide the main window
-
-# Create a system tray icon
-img = Image.open("../images/battery.png")  # Replace with your default battery icon path
-img = img.resize((16, 16), Image.ANTIALIAS)
-icon = ImageTk.PhotoImage(img)
-
-tray_icon = tk.Label(root, image=icon)
-tray_icon.pack()
-tray_icon.after(1000, update_icon)  # Update icon after 1 second to show initial status
-
-# Run the application
-root.mainloop()
+###############################################################################
+if __name__ == '__main__':
+    app = BatteryTray()
+    gtk.main()
